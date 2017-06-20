12ad1ec27090d314841fdd1c7f275c3e8c124cc8
Jennifer Hail <jennifer@bypine.co>
2017-06-20 18:15:59 -0400

    instagram gallery headings

--- template-parts/section-gallery.php
+++ template-parts/section-gallery.php
@@ -1,11 +1,23 @@
-<div class="<?php if ( is_front_page() ) : ?>home-gallery<?php else : ?>gallery<?php endif; ?> section">
-<?php 
+<div class="<?php if ( is_front_page() ) : ?>home-gallery<?php else : ?>gallery<?php endif; ?>">
+	<div class="gallery-heading">
+		
+		<?php 
 
-	if ( get_sub_field('heading') ) { ?>
+		if ( get_sub_field('heading') ) { ?>
 
-		<h1><?php the_sub_field('heading'); ?></h1>
+			<h1><?php the_sub_field('heading'); ?></h1>
 
-	<?php }
+		<?php }
+
+		if ( get_sub_field('subheading') ) { ?>
+
+			<h4><?php the_sub_field('subheading'); ?></h4>
+
+		<?php }; ?>
+
+	</div>
+
+	<?php
 
 	if ( get_sub_field('gallery_shortcode') ) { 
 
@@ -13,5 +25,5 @@
 		
 	}
 
-?>
+	?>
 </div>
