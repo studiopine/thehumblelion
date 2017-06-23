34f6047201ec31ad57f59cfe8064b38c9929211b
Jennifer Hail <jennifer@bypine.co>
2017-06-22 20:33:50 -0400

    404 page

--- 404.php
+++ 404.php
@@ -13,48 +13,40 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'lion' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'lion' ); ?></p>
+				<div class="section">
 
-					<?php
-						get_search_form();
+					<header class="page-header">
+						<h4><?php esc_html_e( 'Oops!', 'lion' ); ?></h4>
+						<h1 class="page-title"><?php esc_html_e( 'That page can&rsquo;t be found.', 'lion' ); ?></h1>
+					</header><!-- .page-header -->
 
-						the_widget( 'WP_Widget_Recent_Posts' );
+				</div>
 
-						// Only show the widget if site has multiple categories.
-						if ( lion_categorized_blog() ) :
-					?>
+				<div class="form-wrapper">
+					
+					<?php 
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'lion' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+					if ( get_sub_field('subheading') ) { ?>
 
-					<?php
-						endif;
+						<h4><?php the_sub_field('subheading'); ?></h4>
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'lion' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+					<?php }
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					if ( get_sub_field('heading') ) { ?>
 
-				</div><!-- .page-content -->
+						<h1><?php the_sub_field('heading'); ?></h1>
+
+					<?php } ?>
+
+					<div class="thl-form">
+						<?php get_search_form(); ?>
+					</div>
+
+				</div>
+
+				<?php get_template_part( 'template-parts/content', 'featured' ); ?><!-- #featured-posts -->
+
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
