d9fdfcf3934cf515fdb2dc9597b598e5e7f863c6
Jennifer Hail <jennifer@bypine.co>
2017-07-22 00:32:00 -0400

    update jetpack share btns

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -55,15 +55,15 @@ function jeherve_remove_all_jp_css() {
   wp_deregister_style( 'infinity-twentyeleven' ); // Infinite Scroll - Twentyeleven Theme
   wp_deregister_style( 'infinity-twentytwelve' ); // Infinite Scroll - Twentytwelve Theme
   wp_deregister_style( 'noticons' ); // Notes
-  wp_deregister_style( 'post-by-email' ); // Post by Email
+  // wp_deregister_style( 'post-by-email' ); // Post by Email
   wp_deregister_style( 'publicize' ); // Publicize
-  wp_deregister_style( 'sharedaddy' ); // Sharedaddy
-  wp_deregister_style( 'sharing' ); // Sharedaddy Sharing
+  // wp_deregister_style( 'sharedaddy' ); // Sharedaddy
+  // wp_deregister_style( 'sharing' ); // Sharedaddy Sharing
   wp_deregister_style( 'stats_reports_css' ); // Stats
   wp_deregister_style( 'jetpack-widgets' ); // Widgets
   wp_deregister_style( 'jetpack-slideshow' ); // Slideshows
   wp_deregister_style( 'presentations' ); // Presentation shortcode
-  wp_deregister_style( 'jetpack-subscriptions' ); // Subscriptions
+  // wp_deregister_style( 'jetpack-subscriptions' ); // Subscriptions
   wp_deregister_style( 'tiled-gallery' ); // Tiled Galleries
   wp_deregister_style( 'widget-conditions' ); // Widget Visibility
   wp_deregister_style( 'jetpack_display_posts_widget' ); // Display Posts Widget
